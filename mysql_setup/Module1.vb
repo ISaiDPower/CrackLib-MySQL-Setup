5bdb1a6be2e3c4d1e0577b73b931a341916f019b
ISaiDPower <treisudest3000@gmail.com>
2021-01-05 23:28:08 +0200

    Add project files.

---
+++ mysql_setup/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Not pro LGBT.")
+        System.Threading.Thread.Sleep(200)
+        Console.Clear()
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("_________                       __   .____    ._____.    ")
+        Console.ForegroundColor = ConsoleColor.DarkYellow
+        Console.WriteLine("\_   ___ \____________    ____ |  | _|    |   |__\_ |__  ")
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.WriteLine("/    \  \/\_  __ \__  \ _/ ___\|  |/ /    |   |  || __ \ ")
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("\     \____|  | \// __ \\  \___|    <|    |___|  || \_\ \")
+        Console.ForegroundColor = ConsoleColor.Cyan
+        Console.WriteLine(" \______  /|__|  (____  /\___  >__|_ \_______ \__||___  /")
+        Console.ForegroundColor = ConsoleColor.DarkCyan
+        Console.WriteLine("        \/            \/     \/     \/       \/       \/ ")
+        Console.ResetColor()
+        Console.BackgroundColor = ConsoleColor.Red
+        Console.WriteLine("WARNING: If you are not using this software with a fresh created database, you shoudn't take the risk and do a full database backup.")
+        Console.Read()
+    End Sub
+
+End Module
---
+++ mysql_setup/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("mysql_setup")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("mysql_setup")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e14dd2c8-01ec-49bb-b32a-0c12585cb995")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
